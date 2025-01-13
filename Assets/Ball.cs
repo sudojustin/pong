b3d4c2627ae2c1a0e433bb60a5b4020c1e2736c8
sudojustin <120149080+sudojustin@users.noreply.github.com>
2025-01-13 10:53:52 -0800

    Implemented initial ball and paddle movements

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
 
-public class Ball : MonoBehaviour
-{
+public class Ball : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+
+    float radius;
+    Vector2 direction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start() {
+        direction = Vector2.one.normalized;  // direction is (1, 1) normalized
+        radius = transform.localScale.x / 2; // half the width
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        transform.Translate (direction * speed * Time.deltaTime);
+
+        // Bounce off top and bottom
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0) {
+            direction.y = -direction.y;
+        }
+        if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0) {
+            direction.y = -direction.y;
+        }
+
+        // Game over
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0) {
+            Debug.Log ("Right player wins!");
+
+            // For now, just freeze time
+            Time.timeScale = 0;
+            enabled = false; // Stop updating the script
+        }
+        if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0) {
+            Debug.Log ("Left player wins!");
+
+            // For now, just freeze time
+            Time.timeScale = 0;
+            enabled = false; // Stop updating the script
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Paddle") {
+            bool isRight = other.GetComponent<Paddle> ().isRight;
+
+            // If hitting right paddle and moving right, flip direction
+            if (isRight == true && direction.x > 0) {
+                direction.x = -direction.x;
+            }
+            // If hitting left paddle and moving left, flip direction
+            if (isRight == false && direction.x < 0) {
+                direction.x = -direction.x;
+            }
+        }
     }
 }
--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
     
+    [SerializeField]
     float speed;
+
     float height;
 
     string input;
-    bool isRight;
+    public bool isRight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -42,6 +44,16 @@ public class Paddle : MonoBehaviour {
 
         // GetAxis is a number between -1 to 1 (-1 for down, 1 for up)
         float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // Restrict paddle movement
+        // If paddle is too low and user is continuing to move down, stop
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0) {
+            move = 0;
+        }
+        // If paddle is too high and user is continuing to move up, stop
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0) {
+            move = 0;
+        }
 
         transform.Translate (move * Vector2.up);
     }
